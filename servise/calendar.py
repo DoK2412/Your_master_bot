e82d3b1c9881d8dce855df95c13bc3afbe48353d
d.gorodenko <d.gorodenko@neurone.ru>
2024-05-06 11:40:14 +0300

    добавление UserPanel

--- servise/calendar.py
+++ servise/calendar.py
@@ -2,10 +2,6 @@ import datetime
 
 
 from telegram_bot_calendar import WYearTelegramCalendar
-from sqlmodel import Session, select
-
-from database.connection_db import engin
-from database.sql_requests import Calendar
 
 
 def get_year(bot, message):
@@ -14,16 +10,3 @@ def get_year(bot, message):
     bot.send_message(message.from_user.id,
                           'Укажите месяц.',
                           reply_markup=calendar)
-
-# def get_days_for_calendar(month, user):
-#
-#     list_day = list()
-#     month = str(month.split('_')[-2])
-#
-#     with Session(engin) as session:
-#         all_day = session.exec(select(Calendar).where(Calendar.month == month, Calendar.actively==True)).all()
-#     for day in all_day:
-#         list_day.append(day.day[:2])
-#     user.day = True
-#
-#     return list_day
